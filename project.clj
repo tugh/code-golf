ec490dcb64d160ab7ca774cbd43077fda42d85bf
tugh <tugh@users.noreply.github.com>
2022-05-11 16:18:28 +0300

    Remove unnecessary entries from gitignore and switch from lein to deps

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject pftv "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.combinatorics "0.1.6"]
-                 [criterium "0.4.6"]]
-  :target-path "target/%s")
