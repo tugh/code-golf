fc8f0301c356bc766155b4ce7288e21742e7f301
tugh <tugh@users.noreply.github.com>
2022-05-11 16:01:29 +0300

    Remove entrypoint and profiles, add criterium benchmark lib dependency

--- project.clj
+++ project.clj
@@ -4,7 +4,6 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/math.combinatorics "0.1.6"]]
-  :main ^:skip-aot pftv.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [criterium "0.4.6"]]
+  :target-path "target/%s")
