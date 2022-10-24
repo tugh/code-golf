b13353adfa73ecef97d0bbf107fd56f05fd0283b
tugh <tugh@users.noreply.github.com>
2022-10-25 01:42:41 +0300

    Another approach for PF.tv #417

--- src/pftv/417.clj
+++ src/pftv/417.clj
@@ -1,4 +1,6 @@
-(ns pftv.417)
+(ns pftv.417
+  (:require
+   [clojure.set :as set]))
 
 (defn index-map
   [v]
@@ -7,3 +9,12 @@
        (group-by second)
        (map (fn [[x y]] [x (->> y (map first) set)]))
        (into {})))
+
+(defn index-map-v2
+  [v]
+  (loop [[x & v] v
+         i 0
+         acc {}]
+    (if (nil? x)
+      acc
+      (recur v (inc i) (update acc x set/union #{i})))))
--- test/pftv/417_test.clj
+++ test/pftv/417_test.clj
@@ -1,11 +1,19 @@
 (ns pftv.417-test
   (:require
    [clojure.test :refer [are deftest testing]]
-   [pftv.417 :refer [index-map]]))
+   [pftv.417 :refer [index-map index-map-v2]]))
 
 (deftest index-map-tests
   (testing "index-map"
     (are [v expected] (= (index-map v) expected)
+      [] {}
+      [1 2 3] {1 #{0} 2 #{1} 3 #{2}}
+      [1 1 1] {1 #{0 1 2}}
+      [1 2 1 2 1] {1 #{0 2 4} 2 #{1 3}})))
+
+(deftest index-map-v2-tests
+  (testing "index-map"
+    (are [v expected] (= (index-map-v2 v) expected)
       [] {}
       [1 2 3] {1 #{0} 2 #{1} 3 #{2}}
       [1 1 1] {1 #{0 1 2}}
