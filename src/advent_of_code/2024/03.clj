6a9128b1e5eb6bd63d6fc34ca4781417aa2799c4
Ertuğrul Erfidan <ertugrul@tureys.com>
2024-12-03 15:54:02 +0300

    Solve day 3 p2

--- src/advent_of_code/2024/03.clj
+++ src/advent_of_code/2024/03.clj
@@ -9,9 +9,30 @@
        (map (fn [[_ x y]] (* (Integer/parseInt x) (Integer/parseInt y))))
        (apply +)))
 
+(defn solve-p2
+  [input]
+  (let [exprs (re-seq #"do\(\)|don\'t\(\)|mul\((\d+)\,(\d+)\)" input)]
+    (loop [[[expr x y] & exprs] exprs
+           enabled? true
+           acc 0]
+      (if (nil? expr)
+        acc
+        (case expr
+          "do()" (recur exprs true acc)
+          "don't()" (recur exprs false acc)
+          (if-not enabled?
+            (recur exprs enabled? acc)
+            (recur exprs enabled? (+ acc (* (Integer/parseInt x)
+                                            (Integer/parseInt y))))))))))
+
+
 (comment
   (-> (io/read-input! "03")
       solve-p1)
   ;;=> 157621318
-  
+
+  (-> (io/read-input! "03")
+      solve-p2)
+  ;;=> 79845780
+
   )
